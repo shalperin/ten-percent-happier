db2a2d799ed519cd891a59879a7d32993f7d7eab
Sam Halperin <sqh@me.com>
2020-01-04 10:28:19 -0500

    backend impl

--- backend/hello.py
+++ backend/hello.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, Response, request, jsonify, make_response
+
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/add', methods=["POST"])
+def add():
+    a = 0
+    b = 0
+    emsg=""
+    try :
+        a = float(request.form['a'])
+        b = float(request.form['b'])
+    except Exception as e:
+        emsg = str(e)
+        
+    if emsg != "":
+       return make_response(jsonify({"exception": emsg}), 400)
+    else:
+        return jsonify({"sum" : a+b})
