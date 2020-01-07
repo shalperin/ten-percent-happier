e62ec8a966c63f1bed95ccd369d2602d02482bf5
Sam Halperin <sqh@me.com>
2020-01-07 10:06:04 -0500

    Mock out backend failure for testing purposes

--- backend/hello.py
+++ backend/hello.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response, request, jsonify, make_response
-
+import random
 import json
+import time
 
 app = Flask(__name__)
 
@@ -19,7 +20,14 @@ def add():
     except Exception as e:
         emsg = str(e)
         
+    mode = random.randint(0,5)
+    print(f"Mode is {mode}")
     if emsg != "":
        return make_response(jsonify({"exception": emsg}), 400)
-    else:
+    elif mode in range(0,4): #add
         return jsonify({"sum" : a+b})
+    elif mode == 4: #timeout
+        time.sleep(30)
+        return jsonify({"exception": "we should have timed out before seeing this data"}, 400 )
+    elif mode == 5: #service unavailable
+        return make_response(jsonify({}), 503)
