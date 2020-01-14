2eaca571021ef8cc12a5486b16921aa233b97518
Sam Halperin <sqh@me.com>
2020-01-14 14:02:31 -0500

    split Resource into its own file.

---
+++ android/Redwood/app/src/main/java/com/blauhaus/android/redwood/Resource.kt
@@ -0,0 +1,11 @@
+package com.blauhaus.android.redwood
+
+
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Loading<T>(data: T? = null, var refreshing: Boolean = false) : Resource<T>(data)
+    class Error<T>(data: T? = null, message: String) : Resource<T>(data, message)
+}
