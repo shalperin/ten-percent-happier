7588e73dfa24d40c270c94dff38ef03e0582c206
Sam Halperin <sqh@me.com>
2020-01-14 10:12:17 -0500

    modified last 4 weeks feature to use vector drawables.
    Can now set colors in XML rather than baked into pngs.

--- android/Redwood/app/src/main/java/com/blauhaus/android/redwood/features/lastfourweeks/views/DotView.kt
+++ android/Redwood/app/src/main/java/com/blauhaus/android/redwood/features/lastfourweeks/views/DotView.kt
@@ -1,15 +1,18 @@
 package com.blauhaus.android.redwood.features.lastfourweeks.views
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.blauhaus.android.redwood.R
 
-class DotView : ImageView {
 
-    //TODO: See if we can use un-tinted vector drawables so that we can style this any way we want.
+class DotView : ImageView {
 
     // Note to self: get more muscle memory of the Code->Generate menu above.
     constructor(context: Context?) : super(context)
@@ -20,17 +23,22 @@ class DotView : ImageView {
         defStyleAttr
     )
 
+    private val metVector = R.drawable.ic_last4weeks_circle_met
+    private val futureVector = R.drawable.ic_last4weeks_circle_future
+    private val metTodayVector = R.drawable.ic_last4weeks_check_circle
+    private val notMetVector = R.drawable.ic_last4weeks_x_mark
+
     private var d: DotViewState =
         DotViewState.Future()
 
     private val metTodayAnim = AnimationUtils.loadAnimation(context, android.R.anim.fade_out).apply{
         setAnimationListener(object: Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation?) {
-                setImageResource(R.drawable.dot_met)
+                setImageResource(metVector)
             }
             override fun onAnimationRepeat(animation: Animation?) {}
             override fun onAnimationEnd(animation: Animation?) {
-                setImageResource(R.drawable.dot_met_today)
+                setImageResource(metTodayVector)
                 startAnimation(AnimationUtils.loadAnimation(context, android.R.anim.fade_in))
             }
         })
@@ -44,19 +52,27 @@ class DotView : ImageView {
     private fun show() {
         when(d) {
             is DotViewState.Skipped -> {
-                setImageResource(R.drawable.dot_skipped)
+                setImageResource(notMetVector)
             }
             is DotViewState.Met -> {
-                setImageResource(R.drawable.dot_met)
+                setImageResource(metVector)
             }
             is DotViewState.MetToday -> {
                 startAnimation(metTodayAnim)
             }
             is DotViewState.Future -> {
-                setImageResource(R.drawable.dot_future)
+                setImageResource(futureVector)
             }
         }
     }
+
+//    fun tintDrawable(drawableR_Id:Int, colorR_Id:Int):Drawable {
+//        var d: Drawable? =
+//            VectorDrawableCompat.create(resources, drawableR_Id, null)
+//        d = DrawableCompat.wrap(d!!)
+//        DrawableCompat.setTint(d, ContextCompat.getColor(context, colorR_Id))
+//        return d
+//    }
 
     sealed class DotViewState() {
         class Skipped() : DotViewState()
@@ -64,4 +80,5 @@ class DotView : ImageView {
         class MetToday(): DotViewState()
         class Future(): DotViewState()
    }
+
 }
