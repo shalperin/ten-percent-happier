5fc43c0fb799b759801021f8e2e76cd2e896d7bb
Sam Halperin <sqh@me.com>
2020-01-15 10:06:05 -0500

    remove initial proof of concept

--- mvvmcalculatorproofofconcept/src/main/java/com/blauhaus/android/mvvmcalculatorproofofconcept/calculator/CalculatorRepo.kt
+++
@@ -1,9 +0,0 @@
-package com.blauhaus.android.mvvmcalculatorproofofconcept.calculator
-
-interface CalculatorRepo {
-    suspend fun sum(a: Float, b:Float): CalculatorModel
-}
-
-class CalculatorRepoImpl(private val calculatorApi: CalculatorApi) :CalculatorRepo {
-    override suspend fun sum(a: Float, b:Float) = calculatorApi.postAdd(a, b)
-}
