787cc7f10ba40cbecb717f1f42f911dc27ecdd80
Sam Halperin <sqh@me.com>
2020-01-27 12:31:35 -0500

    remove old demos

--- sample/src/main/java/com/blauhaus/android/redwood/sample/components/index/IndexFragment.kt
+++ sample/src/main/java/com/blauhaus/android/redwood/sample/components/index/IndexFragment.kt
@@ -1,13 +1,11 @@
-package com.blauhaus.android.redwood.sample.fragments
+package com.blauhaus.android.redwood.sample.components.index
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import com.blauhaus.android.redwood.sample.MainActivity
 import com.blauhaus.android.redwood.sample.R
 import kotlinx.android.synthetic.main.fragment_index.*
 
@@ -26,14 +24,6 @@ class IndexFragment : Fragment() {
         // Set up our menu links
         link_meditationChallenge.setOnClickListener{
             findNavController().navigate(R.id.action_indexFragment_to_meditationDemoFragment)
-        }
-
-        link_4WeeksDemo.setOnClickListener{
-            findNavController().navigate(R.id.action_indexFragment_to_lastFourWeeksDemo1Fragment)
-        }
-
-        link_barChartDemo.setOnClickListener{
-            findNavController().navigate(R.id.action_indexFragment_to_barGraphDemo1Fragment)
         }
 
     }
