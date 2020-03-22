4f56846512063528bb0c6c2d37da9015311d3b52
Sam Halperin <sqh@me.com>
2020-03-22 15:13:41 -0400

    Use new Models with for demo data
    See commit about not using anonymous Pair-based data structures, and instead using Models with named attributes.

--- sample/src/main/java/com/blauhaus/android/redwood/sample/data/DemoData.kt
+++ sample/src/main/java/com/blauhaus/android/redwood/sample/data/DemoData.kt
@@ -1,46 +1,136 @@
 package com.blauhaus.android.redwood.sample.data
 
 import com.blauhaus.android.redwood.sample.R
-import com.blauhaus.android.redwood.sample.data.models.Content
 
 val meditationHistoryData= listOf(
-   // TODO refactor These should be objects with logical names for 'first' and 'second' and probably more flexible
-    // string interpolation for the string.
-    Pair(5f, "5 min @ Jan 1"),
-    Pair(10f, "10 min @ Jan 2"),
-    Pair(2f, "2 min @ Jan 3"),
-    Pair(30f, "30 min @ Jan 4"),
-    Pair(15f, "15 min @ Jan 5"),
-    Pair(12f, "12 min @ Jan 6"),
-    Pair(5f, "5 min @ Jan 7"),
-    Pair(25f, "25 min @ Jan 8"),
-    Pair(0f, "0 min @ Jan 9"),
-    Pair(0f, "0 min @ Jan 10"),
-    Pair(15f, "15 min @ Jan 11"),
-    Pair(15f, "15 min @ Jan 12"),
-    Pair(25f, "25 min @ Jan 13"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(0f, "0 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(20f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(0f, "0 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(0f, "0 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(20f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14"),
-    Pair(35f, "35 min @ Jan 14")
+    ADaysWorthOfMeditationData(
+        5f,
+        "5 min @ Jan 1"
+    ),
+    ADaysWorthOfMeditationData(
+        10f,
+        "10 min @ Jan 2"
+    ),
+    ADaysWorthOfMeditationData(
+        2f,
+        "2 min @ Jan 3"
+    ),
+    ADaysWorthOfMeditationData(
+        30f,
+        "30 min @ Jan 4"
+    ),
+    ADaysWorthOfMeditationData(
+        15f,
+        "15 min @ Jan 5"
+    ),
+    ADaysWorthOfMeditationData(
+        12f,
+        "12 min @ Jan 6"
+    ),
+    ADaysWorthOfMeditationData(
+        5f,
+        "5 min @ Jan 7"
+    ),
+    ADaysWorthOfMeditationData(
+        25f,
+        "25 min @ Jan 8"
+    ),
+    ADaysWorthOfMeditationData(
+        0f,
+        "0 min @ Jan 9"
+    ),
+    ADaysWorthOfMeditationData(
+        0f,
+        "0 min @ Jan 10"
+    ),
+    ADaysWorthOfMeditationData(
+        15f,
+        "15 min @ Jan 11"
+    ),
+    ADaysWorthOfMeditationData(
+        15f,
+        "15 min @ Jan 12"
+    ),
+    ADaysWorthOfMeditationData(
+        25f,
+        "25 min @ Jan 13"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        0f,
+        "0 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        20f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        0f,
+        "0 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        0f,
+        "0 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        20f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    ),
+    ADaysWorthOfMeditationData(
+        35f,
+        "35 min @ Jan 14"
+    )
     )
 
 val myCircleData = listOf(
     // Just grabbing random demo data here.
-    Pair("Dan", meditationHistoryData.subList(2, 8)),
-    Pair("Alexis", meditationHistoryData.subList(4,14))
+    MeditationDataByPersonName(
+        "Dan",
+        meditationHistoryData.subList(2, 8)
+    ),
+    MeditationDataByPersonName(
+        "Alexis",
+        meditationHistoryData.subList(4, 14)
+    )
 )
 
 // random
@@ -48,16 +138,16 @@ val globalStatsData = listOf(54580, 12707803, 19) //[participants, minutes, aver
 
 
 // nice use of named fields.
-val currentSessionData = Content (
+val currentSessionData = Content(
     image = R.drawable.josephgoldstein,
-    title  = "Meditate With Joseph",
+    title = "Meditate With Joseph",
     pretitle = "UP NEXT",
     description = "A beginners mindfulness medition focusing on breath following with Joseph Goldstein.  Joseph Goldstein is founder of the Insight Meditation Society in Barre Mass.",
     videoId = "O44d1U7aYAo",
     audioId = R.raw.goldstein_audio
-    )
+)
 
-val challengeUpdateData = Content (
+val challengeUpdateData = Content(
     image = R.drawable.dog_party_hat,
     title = "The challenge is over, but you just got started!",
     pretitle = "CONGRATS",
